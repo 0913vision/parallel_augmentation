b5f39e2816308cb5b72b3ce7f1ddafa981e7fcb6
0913vision <0913vision@gmail.com>
2024-06-09 17:42:12 +0900

    [#37-5] debug2

--- cpp/getost.cc
+++ cpp/getost.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 extern "C" {
     int get_file_ost(const char* file_path) {
+        cout << "Getting OST for file: " << file_path << endl;
         struct llapi_layout *layout = llapi_layout_get_by_path(file_path, 0);
         uint64_t ost_index;
 
@@ -16,6 +17,7 @@ extern "C" {
 
         llapi_layout_comp_use(layout, 1);
         llapi_layout_ost_index_get(layout, 0, &ost_index);
+        cout << "OST index: " << ost_index << endl;
         return ost_index;
     }
 }
